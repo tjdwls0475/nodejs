b11d75be92be390438a9d9cdebd903ae94838e28
harry <tjdwls0475@hanmail.net>
2021-11-19 10:37:02 +0900

    first node

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
+
+const bodyParser = require('body-parser')
+const { application } = require('express')
+
+const app = express()
+app.use(bodyParser.urlencoded({extended: true}))
+const port = process.env.PORT || 5000
+
+const publicDir = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+app.use(express.static(publicDir))
+
+app.get('/', (req, res)=>{
+    res.render('index', {
+        제목: '미세먼지 정보 앱'
+    })
+})
+// app.get('/help', (req,res)=>{
+//     res.send('어떤 도움이 필요하십니까?')
+// })
+// app.get('/about', (req,res)=>{
+//     res.send('express server를 작동하는 application입니다.')
+// })
+
+app.listen(port, ()=>{
+    console.log(`Server is up and running at port ${port}`)
+})
---
+++ src/utils/airdata.js
@@ -0,0 +1,4 @@
+const request = require('request')
+const serviceKey = require('../keys.key')
+
+const airdata = (stationName, )
